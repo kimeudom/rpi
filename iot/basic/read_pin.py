4c7894e6d476e93bedc431a756d9f3f87be71036
Dom <kimeudom.git@gmail.com>
2023-06-13 11:32:06 +0300

    Step up and down

--- iot/basic/read_pin.py
+++ iot/basic/read_pin.py
@@ -1,21 +1,21 @@
 ## reads pin output and relays that back
 
-import RPi.GPIO as gpio
+import RPi.GPIO as GPIO
 from time import sleep
 
 # Board setup
-gpio.setmode(gpio.BOARD)
+gpio.setmode(GPIO.BOARD)
 
 # Pin setup
 inPin = 40
-gpio.setup(inPin, gpio.IN)
+GPIO.setup(inPin, GPIO.IN)
 
 try:
   while 1:
      sleep(1)
-     readVal = gpio.input(inPin)
+     readVal = GPIO.input(inPin)
      print(readVal)
      sleep(1)
 
 except KeyboardInterrupt:
-    gpio.cleanup()
+    GPIO.cleanup()
