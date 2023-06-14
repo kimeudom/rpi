e88832511e4467a828fdb697fe28404333a85331
Dom <kimeudom.git@gmail.com>
2023-06-14 11:46:28 +0300

    Internal push up and push down resistors

---
+++ iot/basic/internal_push_down.py
@@ -0,0 +1,24 @@
+# This code utilizes the internal step up functionality of the RPi
+# There is no need to utilize breadboad step up resistors
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+# Board setup mode
+GPIO.setmode(GPIO.BOARD)
+
+# The input pin for the push button read
+inPin = 40
+
+# setting up the button
+GPIO.setup(inPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+try: 
+  while True:
+    sleep(0.1)
+    if(GPIO.input(inPin) == 1):
+      print(GPIO.input(inPin))
+
+except KeyboardInterrupt:
+  GPIO.cleanup()
+  print("Exited\n")
