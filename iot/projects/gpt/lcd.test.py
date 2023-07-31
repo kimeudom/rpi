a2986903fc57ba113b94ae3278a458077040566c
dom <kimeudom.git@gmail.com>
2023-07-31 22:32:05 +0300

    2004 tests

--- iot/projects/gpt/lcd.test.py
+++ iot/projects/gpt/lcd.test.py
@@ -1,3 +1,16 @@
 import RPi.GPIO as GPIO
-import LCD1602
+from RPLCD import *
+from RPLCD.i2c import CharLCD
 from time import sleep
+
+lcd = CharLCD('PCF8574',0x27)
+lcd.cursor_pos = (0,0)
+
+try:
+  lcd.write_string("Hello there")
+
+except KeyboardInterrupt:
+ sleep(.2)
+ lcd.clear()
+ GPIO.cleanup()
+ print("\nExiting...\n")
